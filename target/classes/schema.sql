7a904681b201996e6a1af826ee30f079e389f10f
Roman Grek <roman.grek0098@gmail.com>
2020-04-01 22:53:58 +0300

    Deploy to Heroku

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-create table courses
-(
-   id integer not null,
-   name varchar(255) not null,
-   language varchar(255) not null,
-   level varchar(255) not null,
-   price integer not null,
-   primary key(id)
-);
